240dea03c12eacc210a08fbff9bc9dd57b9fed6c
sworc <lightbox@nifty.com>
2021-11-14 16:07:07 +0900

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,12 @@
+REM access フォルダ
+MOVE access\*.* \app\workspace
+
+REM csv フォルダ
+mkdir \app\workspace\csv
+MOVE csv\*.* \app\workspace\csv
+
+REM excel フォルダ
+MOVE excel\*.* \app\workspace
+
+REM sqlite3
+MOVE sqlite3\*.* \app\workspace
