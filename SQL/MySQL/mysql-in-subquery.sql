3f3209ffc4e323b512674fec780f3543f50c43e9
sworc <lightbox@nifty.com>
2021-12-09 15:59:15 +0900

    Add files via upload

---
+++ SQL/MySQL/mysql-in-subquery.sql
@@ -0,0 +1,23 @@
+-- 売上額 400万以上の得意策の担当者の一覧
+SELECT
+    *
+FROM
+    社員マスタ
+WHERE
+    社員コード IN (
+        SELECT
+            担当者
+        FROM
+            得意先マスタ
+        WHERE
+            得意先コード IN (
+                SELECT
+                    取引先コード
+                FROM
+                    取引データ
+                GROUP BY
+                    取引先コード
+                HAVING
+                    sum(金額) >= 4000000
+            )
+    )
