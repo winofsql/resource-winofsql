4b99f57144e525e9661066bfb6d674c2513c8efc
sworc <lightbox@nifty.com>
2021-12-10 14:44:40 +0900

    Update mysql-union.sql

--- SQL/MySQL/mysql-union.sql
+++ SQL/MySQL/mysql-union.sql
@@ -20,3 +20,36 @@ FROM
     社員マスタ
 WHERE
     給与 <= 200000;
+    
+SELECT
+    *
+FROM
+    (
+        SELECT
+            社員コード,
+            氏名
+        FROM
+            社員マスタ
+        WHERE
+            性別 = 0
+            AND 給与 >= 250000
+        UNION
+        SELECT
+            社員コード,
+            氏名
+        FROM
+            社員マスタ
+        WHERE
+            性別 = 1
+            AND 給与 >= 250000
+        UNION
+        SELECT
+            社員コード,
+            氏名
+        FROM
+            社員マスタ
+        WHERE
+            給与 <= 200000
+    ) 一覧
+ORDER BY
+    社員コード;
