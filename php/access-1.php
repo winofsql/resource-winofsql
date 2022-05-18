88b10fa97de51483b469f609a0236eb21a71d765
sworc <lightbox@nifty.com>
2022-05-18 21:49:31 +0900

    Add files via upload

--- php/access-1.php
+++ php/access-1.php
@@ -11,16 +11,17 @@ mb_internal_encoding('UTF-8');
 $dbh = new PDO("odbc:H-ACCDB");
 
 // キャラクタセット変換
-$sql = mb_convert_encoding("SELECT * FROM 社員マスタ", "SJIS", "UTF-8");
+$sql = mb_convert_encoding("SELECT * FROM 社員マスタ", "cp932", "UTF-8");
 
 $stmt = $dbh->prepare($sql);
 $stmt->execute();
 
 // fetch は引数なし
 while( $rows = $stmt->fetch() ){
-    print mb_convert_encoding($rows[0], "UTF-8", "SJIS") . " / ";
+    print mb_convert_encoding($rows[0], "UTF-8", "cp932") . " / ";
+    print mb_convert_encoding($rows[mb_convert_encoding("社員コード","cp932", "UTF-8")], "UTF-8", "cp932") . " / ";
     print mb_convert_encoding($rows[1], "UTF-8", "SJIS") . " / ";
-    print mb_convert_encoding($rows[2], "UTF-8", "SJIS") . "<br>";
+    print mb_convert_encoding($rows[mb_convert_encoding("氏名","cp932", "UTF-8")], "UTF-8", "cp932") . "<br>";
 }
 
 ?>
